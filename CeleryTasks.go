ce4bf6c0a18ab2a7ebf8e9cee7e2fb52f1a7c24d
Dean Liang <dextersquad4@gmail.com>
2026-07-21 19:21:52 -0400

    finish the uni-process celery worker business

--- CeleryTasks.go
+++ CeleryTasks.go
@@ -9,59 +9,109 @@ import (
 type Task struct {
 	id int
 	Func func() (any, error)
+	tries int
+	step string
 }
 
 type Celery struct {
-	Wait chan Task
+	Wait chan *Task
+	Stop chan struct{}
 	NextID int	
 	IDMut sync.Mutex
 	wg sync.WaitGroup
+	taskMap map[int]*Task
+	workerWG sync.WaitGroup
 }
 
 func initCelery(numWorkers int) *Celery {
 	wg := sync.WaitGroup{}
 
 	c:=Celery{
-		Wait: make(chan Task, 100),
+		Wait: make(chan *Task, 100),
+		Stop: make(chan struct{}), 
 		NextID: 0,
 		IDMut: sync.Mutex{},
 		wg: wg,
+		taskMap: make(map[int]*Task),
+		workerWG: sync.WaitGroup{},
 	}
 
 	for i := range numWorkers {
+		c.workerWG.Add(1)
 		go c.initWorker(i)
 	}
 
 	return &c
 } 
 
-func (c *Celery) addTask(f func() (any, error)) (string, error) {
+func (c *Celery) addTask(f func() (any, error)) (int, error) {
 	c.IDMut.Lock()
 	task := Task{
 		id:c.NextID,
 		Func: f,
+		tries: 0,
+		step: "waiting",
 	}
 	c.NextID++
 	c.IDMut.Unlock()
-
-	c.Wait <- task
+	
+	c.taskMap[task.id] = &task
 	c.wg.Add(1)
-	return "Added", nil
+	c.Wait <- &task
+	return task.id, nil
 }
 
 func (c *Celery) initWorker(index int) {
-	for task := range c.Wait {
-		res, e:=task.Func()
-		msg:=""
-		if e != nil {
-			msg=fmt.Sprintf("Worker %d failed in execution %d with result: %v\n",  index, task.id, e.Error())
-		} else {
-			msg=fmt.Sprintf("Worker %d completed %d with result: %v\n", index, task.id, res)
-		}
+	defer func() {
+		c.workerWG.Done()
+	}()
+	for {
+		select {
+			case task:= <-c.Wait:
+				if task.tries >= 3 {
+					task.step="failed"
+					msg:=fmt.Sprintf("Task with id %d failed max amount of times\n", task.id)
+					fmt.Println(msg)
+					c.wg.Done()
+					continue
+				}
 
-		fmt.Println(msg)
-		c.wg.Done()
+
+				task.step = "running"
+				res, e:= task.Func()
+				msg:=""
+				if e != nil {
+					msg=fmt.Sprintf("Worker %d failed in execution of task with id %d with result: %v\n",  index, task.id, e.Error())
+					task.step="waiting"
+					task.tries++
+					c.Wait<-task
+					fmt.Println(msg)
+				} else {
+					msg=fmt.Sprintf("Worker %d completed %d with result: %v\n", index, task.id, res)
+					task.step="completed"
+					fmt.Println(msg)
+					c.wg.Done()
+				}
+			case <-c.Stop:
+				return 
+		}
 	}
+}
+
+
+func (c *Celery) termWorkers() {
+	close(c.Stop)
+}
+
+func (c *Celery) reinitWorkers(numWorkers int) error {
+	c.workerWG.Wait()
+	c.Stop = make(chan struct{})
+
+	for i := range numWorkers {
+		go c.initWorker(i)
+	}
+
+	return nil
 }
 
 
@@ -70,9 +120,9 @@ func main() {
 	celeryQueue:=initCelery(8)
 	
 	timeStart:=time.Now()
-	celeryQueue.addTask(
+	id1, _:=celeryQueue.addTask(
 		func() (any, error) {
-			piss(2)
+			piss(1)
 			return "Bob", nil
 		},
 	)
@@ -85,10 +135,28 @@ func main() {
 
 	celeryQueue.addTask(
 		func() (any, error) {
-			piss(4)
+			piss(2)
 			return "Duh", nil
 		},
 	)
+
+	id4, _:=celeryQueue.addTask(
+		func() (any, error) {
+			piss(1)
+			return "Duh", nil
+		},
+	)
+
+	fmt.Println(celeryQueue.taskMap[id1].step)
+	fmt.Println(celeryQueue.taskMap[id4].step)
+
+
+	
+	celeryQueue.wg.Wait()
+	fmt.Println(celeryQueue.taskMap[id4].step)
+
+	celeryQueue.termWorkers()
+	celeryQueue.reinitWorkers(8)
 
 	celeryQueue.addTask(
 		func() (any, error) {
@@ -96,12 +164,11 @@ func main() {
 			return "Duh", nil
 		},
 	)
-	
 	celeryQueue.wg.Wait()
 
-	fmt.Println("This took %v secs", time.Since(timeStart))
+	fmt.Println("This took", time.Since(timeStart))
 }
 
-func piss(secs int64) {
+func piss(secs int) {
 	time.Sleep(time.Second * time.Duration(secs))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,4 @@
 module github.com/deanliang06/CeleryTasks
 
 go 1.26.5
+
